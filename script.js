f031c8c8657ccfaa01cf97449e4248bc9f630c33
Engineer-Collo <collinslikhomba@gmail.com>
2024-12-31 18:46:43 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 
+const display = document.querySelector('#displayField');
+
+
+
+
+
+
+
+function addValueToDisplay(parvalue) {
+display.value = display.value + parvalue;
+}
+
+function clearDisplayValue() {
+    display.value = "";
+    }
+   
+    function calculateDisplayValues() {
+        try{
+        display.value = eval(display.value);
+        }catch(error) {
+            display.value = "error";
+        }
+        }
